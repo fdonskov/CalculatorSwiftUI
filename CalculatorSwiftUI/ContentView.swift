08be1deca891833f7fe18f60fe812e503eaa8d84
fdonskov <fedya_donskov@mail.ru>
2022-11-12 15:05:10 +0300

    Made the calculator application interface

--- CalculatorSwiftUI/ContentView.swift
+++ CalculatorSwiftUI/ContentView.swift
@@ -8,14 +8,45 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let buttons = [
+        ["7", "8", "9", "X"],
+        ["4", "5", "6", "-"],
+        ["1", "2", "3", "+"],
+        ["0", ".", ".", "="]
+    ]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        
+        ZStack(alignment: .bottom) {
+            Color.black.edgesIgnoringSafeArea(.all)
+            
+            VStack(spacing: 12) {
+                
+                HStack {
+                    Spacer()
+                    Text("42").foregroundColor(.white)
+                        .font(.system(size: 64))
+                }.padding()
+                
+                ForEach(buttons, id: \.self) { row in
+                    HStack(spacing: 12) {
+                        ForEach(row, id: \.self) { button in
+                            Text(button)
+                                .font(.system(size: 32))
+                                .frame(width: self.bottomWidth(), height: self.bottomWidth())
+                                .foregroundColor(.white)
+                                .background(Color.yellow)
+                                .cornerRadius(self.bottomWidth())
+                        }
+                    }
+                }
+            }.padding(.bottom)
         }
-        .padding()
+    }
+    
+    func bottomWidth() -> CGFloat {
+        return (UIScreen.main.bounds.width - 5 * 12) / 4
     }
 }
 
