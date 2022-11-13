1287ad65c999fb464014b3be368a0ab28c1df82d
fdonskov <fedya_donskov@mail.ru>
2022-11-13 08:55:04 +0300

    Added button action display

--- CalculatorSwiftUI/ContentView.swift
+++ CalculatorSwiftUI/ContentView.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 
 enum CalculatorButton: String {
-    case zero, one, two, three, four, five, six, seven
+    case zero, one, two, three, four, five, six, seven, eight, nine
     case equals, plus, minus, multily, divide
     case decimal
     case ac, plusMinus, percent
@@ -23,6 +23,8 @@ enum CalculatorButton: String {
         case .five: return "5"
         case .six: return "6"
         case .seven: return "7"
+        case .eight: return "8"
+        case .nine: return "9"
         case .plus: return "+"
         case .minus: return "-"
         case .multily: return "X"
@@ -37,7 +39,7 @@ enum CalculatorButton: String {
     
     var backgroundColor: Color {
         switch self {
-        case .zero, .one, .two, .three, .four, .five, .six, .seven:
+        case .zero, .one, .two, .three, .four, .five, .six, .seven, .eight, .nine:
             return Color(.darkGray)
         case .ac, .plusMinus, .percent:
             return Color(.lightGray)
@@ -47,11 +49,23 @@ enum CalculatorButton: String {
     }
 }
 
+// Global Application State
+class GlovalEnvironment: ObservableObject {
+    
+    @Published var display = ""
+    
+    func receiveInput(calculatorButton: CalculatorButton) {
+        self.display = calculatorButton.title
+    }
+}
+
 struct ContentView: View {
+    
+    @EnvironmentObject var env: GlovalEnvironment
     
     let buttons: [[CalculatorButton]] = [
         [.ac, .plusMinus, .percent, .divide],
-        [.seven, .five, .six, .minus],
+        [.seven, .eight, .nine, .multily],
         [.four, .five, .six, .minus],
         [.one, .two, .three, .plus],
         [.zero, .decimal, .equals]
@@ -66,43 +80,51 @@ struct ContentView: View {
                 
                 HStack {
                     Spacer()
-                    Text("42").foregroundColor(.white)
+                    Text(env.display).foregroundColor(.white)
                         .font(.system(size: 64))
                 }.padding()
                 
                 ForEach(buttons, id: \.self) { row in
                     HStack(spacing: 12) {
                         ForEach(row, id: \.self) { button in
-                            
-                            Button(action: {
-                                
-                            }) {
-                                Text(button.title)
-                                    .font(.system(size: 32))
-                                    .frame(width: self.bottomWidth(button: button), height: (UIScreen.main.bounds.width - 5 * 12) / 4)
-                                    .foregroundColor(.white)
-                                    .background(button.backgroundColor)
-                                    .cornerRadius(self.bottomWidth(button: button))
-                            }
+                          CalculatorButtonView(button: button)
                         }
                     }
                 }
             }.padding(.bottom)
         }
     }
+}
+
+struct CalculatorButtonView: View {
     
-    func bottomWidth(button: CalculatorButton) -> CGFloat {
-        
+    var button: CalculatorButton
+    
+    @EnvironmentObject var env: GlovalEnvironment
+    
+    var body: some View {
+        Button(action: {
+            self.env.receiveInput(calculatorButton: button)
+        }) {
+            Text(button.title)
+                .font(.system(size: 32))
+                .frame(width: self.bottomWidth(button: button), height: (UIScreen.main.bounds.width - 5 * 12) / 4)
+                .foregroundColor(.white)
+                .background(button.backgroundColor)
+                .cornerRadius(self.bottomWidth(button: button))
+        }
+    }
+    
+    private func bottomWidth(button: CalculatorButton) -> CGFloat {
         if button == .zero {
             return (UIScreen.main.bounds.width - 4 * 12) / 4 * 2
         }
-        
         return (UIScreen.main.bounds.width - 5 * 12) / 4
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView().environmentObject(GlovalEnvironment())
     }
 }
